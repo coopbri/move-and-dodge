27fc97b0491ec853e4abc8efbc8625427aa55961
brian <brian@brian-cooper.com>
2019-11-24 23:08:33 -0600

    add project files
    Former-commit-id: 79de9ccffa05b39525e1c04424582574d0b4da7d

---
+++ scripts/Counter.gd
@@ -0,0 +1,15 @@
+extends Label
+
+var coins = 0
+
+func _ready():
+	text = String(coins)
+
+func _on_coin_collected():
+	coins = coins + 1
+	_ready()
+	if coins == 5:
+		$Timer.start()
+
+func _on_Timer_timeout():
+	get_tree().change_scene("res://YouWin.tscn")
